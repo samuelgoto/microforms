e72b2e8bbfa2ef9575b6e46ba8dc3d734145a77d
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-07-19 16:26:28 -0700

    adds a disabled property to <form>

--- form.md
+++ form.md
@@ -7,6 +7,7 @@ The &lt;form&gt; tag defines a templated hypertext transition.
 | Attribute        | Type           | Description  |
 | :--------------- |:---------------| :------------|
 | action | URL | Specifies where to send the form-data when a form is submitted |
+| disabled | disabled | Specifies that the should be disabled |
 | enctype | ```application/x-www-form-urlencoded```, ```multipart/form-data``` or ```text/plain``` | Specifies how the form-data should be encoded when submitting it to the server (only for method='post') |
 | method | ```get```, ```post``` ```delete``` or ```patch``` | Specifies the HTTP method to use when sending form-data |
 | name | string | Specifies the form name |
--- schema.js
+++ schema.js
@@ -33,6 +33,7 @@ let nodes =
        `The <form> tag defines a templated hypertext transition.`
        ].map(x => x.trim()).join("\n\n"), 
        [prop("action", "URL", "Specifies where to send the form-data when a form is submitted"),
+        prop("disabled", "disabled", "Specifies that the should be disabled"),
         prop("enctype", "```application/x-www-form-urlencoded```, ```multipart/form-data``` or ```text/plain```", "Specifies how the form-data should be encoded when submitting it to the server (only for method='post')"),
         prop("method", "```get```, ```post``` ```delete``` or ```patch```", "Specifies the HTTP method to use when sending form-data"),
         prop("name", "string", "Specifies the form name"),
